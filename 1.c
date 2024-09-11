62b1b0d62b55d69144c1f864613d9299a902acd9
Tirth S Pandya <teamfznltd@gmail.com>
2024-09-11 21:13:23 +0530

    commit

--- 1.c
+++ 1.c
@@ -1,15 +1,23 @@
-// find factorial using a for loop
+// // find factorial using a for loop
+// #include<stdio.h>
+// int main(){
+//     int i, num;
+// printf("Enter a number: ");
+// scanf("%d", &num);
+// int factorial = 1;
+// for(i = 1; i <= num; i++){
+//     factorial *= i;
+// }
+// printf("Factorial of %d is %d\n", num, factorial);
+
+
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int i, num;
-printf("Enter a number: ");
-scanf("%d", &num);
-int factorial = 1;
-for(i = 1; i <= num; i++){
-    factorial *= i;
-}
-printf("Factorial of %d is %d\n", num, factorial);
-
+    int x;
+    printf("%d\n", x);
 
     return 0;
 }
---
+++ notexample.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("%d", x);
+
+    return 0;
+}
